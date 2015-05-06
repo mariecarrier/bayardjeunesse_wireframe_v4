02c9eb1f720a79187699d900bbb7335c8db1ccf3
Marie-Michèle <mm.carrier@bayardcanada.com>
2015-05-06 12:39:46 -0400

    Modifications de la page suggestion de produits.

---
+++ include-boutique/listing-produit-suggestions.php
@@ -0,0 +1,27 @@
+
+    <div class="col-lg-3 image-produit">
+        <div class="wrapper-cover-content">
+            <div class="margin-cover-content">
+                <div class="wrapper-covers-content">
+                    <div class="table-w">
+                           <img src="img/deb_cover.jpg" style=" width: 100%;">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-listing">
+            <h2>Les Débrouillards</h2>
+            <div class="sub regular age">Science et découverte</div>
+            <div class="sub regular age">9 à 14 ans</div>
+            <div class="sub plus">41.95$</div>
+            <div class="wrap-large-but" id="je-labonne">
+                <a href="/wireframe-v1/include-boutique/page-produit.php"><div class="subscribenow content bouton-achat"  id="je-labonne">
+                    <input type="button" value="Rajouter au panier" class="call"/>
+                </div>
+                </a>
+            </div>
+            <div class="sub regular age" style=" color:red; font-weight: bold;">Avec l'abonnement de 2 ans, obtenez un livre!</div>
+        </div>
+    </div>
+
+
--- include-boutique/panier-suggestions-produits.php
+++ include-boutique/panier-suggestions-produits.php
@@ -4,15 +4,236 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Page produit</title>
-<link href="css/custom.css" rel="stylesheet">
+<title>Wireframe BJ 1</title>
+<link href="css/custom-page-suggestion-produits.css" rel="stylesheet">
 <link href="css/bootstrap.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+
+
+
 </head>
-<body>
-<?php
-        include 'header.php';
-  ?>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container header">
+    
+    
+    
+    
+     <div class="container logo navbar-header large">
+            <div class="wrapper-logo">
+                <div class="table-w" id="page-accueil-content">
+                 <a href="/">
+                    <img src="img/logo_bj_large.png">
+                   </a> 
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="container logo navbar-header petit">
+            <div class="wrapper-logo">
+                <div class="table-w" id="page-accueil-content">
+                 <a href="/">
+                    <img src="img/logo_bj_petit.png">
+                   </a> 
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="container logo navbar-header mini">
+            <div class="wrapper-logo">
+                <div class="table-w" id="page-accueil-content">
+                 <a href="/">
+                    <img src="img/logo_bj_petit.png">
+                   </a> 
+                </div>
+            </div>
+        </div>
+        
+        
+        <!--<div id="navbar" class="container gauche navbar-collapse collapse"> -->
+        
+         <div id="" class="container gauche">
+            <div class="header-gauche">
+            <div class="wrapper-header-gauche reseaux-sociaux" style="padding-top: 0px;">
+             <div>Suivez-nous F T</div> 
+            </div>
+            <div style="clear:both"></div>
+                <div class="wrapper-header-gauche">
+            
+                
+                <div class="td td-loupe">
+                <img src="img/icone-loupe.png">
+                </div>
+                
+                <a href="/include-transact/index.php">
+                <div class="td td-reabo">
+                <img src="img/icone-reabonnement.png">
+                 <p>Réabonnement</p>
+                </div>
+                </a>
+                
+                <a href="/include-transact/index.php">
+                <div class="td td-client">
+                <img src="img/icone-compte-client.png">
+                <p>Compte client</p>
+                </div>
+                </a>
+                
+                 
+                <a href="/include-transact/accueil-service-client.php">
+                <div class="td td-client">
+                <img src="img/icone-service-client.png">
+                <p style="float:none !important;">Service client</p>
+                </div>
+                </a>
+                
+                <a href="/panier-suggestions-produits.php#terminer-commande">
+                <div class="td td-panier">
+                 <img src="img/icone-panier-0.png">
+                </div>
+                </a>
+                
+                </div>
+                
+                
+       
+            </div>
+        </div>
+    
+    
+    
+    
+  
+    
+    
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+           </div>
+           
+             <hr class="ligne-grise">
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-left">
+               <li class="dropdown barre"><a href="/" class="dropdown-toggle"><div class="menu-item">Accueil</div></a>
+               <li class="dropdown barre"><a href="/wireframe-v1/include-boutique/lp-produits.php" class="dropdown-toggle"><div class="menu-item">Magazines 0-5 ans</div></a>
+                    <ul class="dropdown-menu mega-menu">
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Popi | 1-3 ans</a></div></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Tralalire | 2-5 ans</a></div></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Babar | 2-6 ans</a></div></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Pomme | d'Api 3-7 ans</a></div></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Pomme d'Api Soleil | 3-7 ans</a></div></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Les Belles Histoires | 4-8 ans</a></div></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Youpi | 5-8 ans</a></div></li>
+                    </ul>
+                </li>
+                <!--<li class="dropdown barre"> <a href="/wireframe-v1/include-boutique/lp-produits.php" class="dropdown-toggle" data-toggle="dropdown">-->
+                <li class="dropdown barre"> <a href="/wireframe-v1/include-boutique/lp-produits.php" class="dropdown-toggle"><div class="menu-item">Magazines 6-10 ans</div></a>
+                    <ul class="dropdown-menu mega-menu">
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Youpi | 5-8 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Mes premiers J'aime lire | 6 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">J'aime lire | 7-10 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Astrapi | 7-11 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Les Explorateurs | 6-10 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Images Doc | 8-12 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Filotéo | 8-13 ans</a></li>
+                        
+                         <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Prions en Eglise Junior | 8-13 ans</a></li>
+                         
+                          <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">I Love English mini</a></li>
+                          
+                          <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">J'aime lire Max | 9-13 ans</a></li>
+                          
+                          <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Je Bouquine | 10-15 ans</a></li>
+                          
+                          <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Okapi | 10-15 ans</a></li>
+                          
+                          
+                    </ul>
+                </li>
+                <li class="dropdown barre" id="mag-0-11-ans"> 
+                    <!-- MM - ORIGINAL <a href="http://127.0.0.32/magazines-11-ans-et-plus" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>--> 
+                    
+                    <!--MM - DÉBUT - NOUVEAU --> 
+                    <a href="/wireframe-v1/include-boutique/lp-produits.php"><div class="menu-item">Magazines 11 ans et +</div></a> 
+                    <!--MM - FIN - NOUVEAU -->
+                    
+                    <ul class="dropdown-menu mega-menu">
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Images Doc | 8-12 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">J'aime lire Max | 9-13 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Les Débrouillards | 9-14 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Je Bouquine | 10-15 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Okapi | 10-15 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">I Love English | 12-15 ans</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Phosphore | 14 ans et +</a></li>
+                        
+                            <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Muze | 15 ans et +</a></li>
+                            
+                                <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Today in English 15 ans et +</a></li>
+                                
+                                    <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Curium | Dès 14 ans</a></li>
+                        
+                    </ul>
+                </li>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                <li class="dropdown barre"> 
+                    <!-- MM - ORIGINAL <a href="http://127.0.0.32/magazines-11-ans-et-plus" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>--> 
+                    
+                    <!--MM - DÉBUT - NOUVEAU --> 
+                    <a href="/wireframe-v1/include-boutique/lp-produits.php"><div class="menu-item">Livres</div></a> 
+                    <!--MM - FIN - NOUVEAU -->
+                    
+                    <ul class="dropdown-menu mega-menu">
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">???</a></li>
+                        
+                    </ul>
+                </li>
+                
+                <li class="dropdown barre"> 
+                    <!-- MM - ORIGINAL <a href="http://127.0.0.32/magazines-11-ans-et-plus" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>--> 
+                    
+                    <!--MM - DÉBUT - NOUVEAU --> 
+                    <a href="/wireframe-v1/include-boutique/lp-produits.php"><div class="menu-item">Idées cadeaux</div></a> 
+                    <!--MM - FIN - NOUVEAU -->
+                    
+                    <ul class="dropdown-menu mega-menu">
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Accessoires</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Casse-têtes</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">DVD</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Éveil à la foi</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Figurines</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Livres à colorier</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Livres-jeux</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Rangements</a></li>
+                        
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Peluches</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Sacs à surprises</a></li>
+                        <li class="bgdrop"><div class="menu-item2"><a href="/wireframe-v1/include-boutique/page-produit.php">Vêtements</a></li>
+                    </ul>
+                </li>
+                
+                
+                
+                
+             
+                
+                
+                
+            </ul>
+        </div>
+        <!--/.navbar-collapse --> 
+    </div>
+</nav>
+
+
 <div class="container content-princ" id="container-content">
 <?php
         //include 'breadcrump.php';
@@ -20,9 +241,9 @@
 
 <div class="wrapper-prec-suivant">
  <div class="header-desc">
-            <h3 style="color: red;">Merci d'avoir rajouté cet article à votre panier</h3>
-            <p>Défilez vers le bas pour TERMINER VOTRE COMMANDE.</p>
-            <p><a href="">Cliquez ici</a> pour revenir à la page précédente.</p>
+            <h4 style="color: green;">Youpi! Vous avez rajouté un article à votre panier.</h4>
+     
+       
         </div>
 
     
@@ -30,12 +251,13 @@
         <h3>Ces articles pourraient aussi vous plaire</h3>
         <div class="row content1">
             <?php 
-include 'listing-produit.php';
+include 'listing-produit-suggestions.php';
 
-include 'listing-produit.php';
+include 'listing-produit-suggestions.php';
 
-include 'listing-produit.php';
+include 'listing-produit-suggestions.php';
 
+include 'listing-produit-suggestions.php';
 
 ?>
         </div>
@@ -197,12 +419,22 @@ include 'listing-produit.php';
     <div class="right-terminer-achat"> </div>
     <div style=" clear:both;"></div>
 
-     
+
+     <div style="margin-top: 50px;">
         <div class="wrap-large-but terminer-commande" id="je-labonne">
                 <a href="/wireframe-v1/include-boutique/include-transact/page-accueil-connexion-achat.php"><div class="subscribenow content bouton-achat" id="je-labonne">
                     <input type="button" value="Terminer ma commande" class="call">
                 </div></a>
       </div>
+      
+       <div class="wrap-large-but continuer-magaziner" id="je-labonne">
+                <a href="/wireframe-v1/include-boutique/index.php"><div class="" id="je-labonne">
+                    <input type="button" value="Continuer à magaziner" class="call">
+                </div></a>
+</div>
+      
+      </div>
+      
     <div style=" clear:both;"></div>
     </div>
 </div>
